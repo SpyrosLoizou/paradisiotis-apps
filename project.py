828f3c3113b8931ce5e0904586e8a21ac854e49f
SpyrosLoizou <34243706+SpyrosLoizou@users.noreply.github.com>
2021-08-16 15:56:32 +0300

    Update project.py

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ import pandas as pd
 import plotly.express as px
 import os
 
-st.sidebar.image("/Data/paralogo.png", use_column_width=True)
+st.sidebar.image("https://github.com/SpyrosLoizou/paradisiotis-apps/tree/main/Data/paralogo.png", use_column_width=True)
 
 
 #Hide streamlit settings
